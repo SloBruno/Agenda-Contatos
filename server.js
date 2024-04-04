e0d39115f900f1f741f13eacac0950c847abf3b8
Bruno Slominski <brunoslominsk@gmail.com>
2024-04-04 16:41:17 -0300

    deploy

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-require('dotenv').config();
+require('dotenv').config(); // Load environment variables from .env file
+
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
@@ -22,9 +23,14 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.resolve(__dirname, 'public')));
 
+const mongoStoreOptions = {
+  mongoUrl: process.env.CONNECTIONSTRING,
+  autoRemove: 'native', // This is optional, adjust according to your needs
+};
+
 const sessionOptions = session({
   secret: 'akasdfj0út23453456+54qt23qv  qwf qwer qwer qewr asdasdasda a6()',
-  store: MongoStore.create({ mongoUrl: process.env.CONNECTIONSTRING }),
+  store: MongoStore.create(mongoStoreOptions),
   resave: false,
   saveUninitialized: false,
   cookie: {
@@ -47,7 +53,7 @@ app.use(routes);
 
 app.on('pronto', () => {
   app.listen(3000, () => {
-    console.log('Acessar http://localhost:3000');
-    console.log('Servidor executando na porta 3000');
+    console.log('Acessar http://localhost:3001');
+    console.log('Servidor executando na porta 3001');
   });
 });
